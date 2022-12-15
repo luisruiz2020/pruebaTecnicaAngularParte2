3d734f9e8c8ce9db07dd76d3b9c9ef886ef6cb41
luruiz22 <luruiz22@uan.edu.co>
2022-12-14 21:48:01 -0500

    Metodos para consumir REST
    Se agregan metodos para obtener la lista de usuarios, consultar por id a un usuario, actualizar un usuario y eliminar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,72 @@ import { catchError, retry } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'prueba';
+  url: string = 'https://jsonplaceholder.typicode.com/';
+  usu = {
+    id: 1,
+    name: 'Leanne Graham 2',
+    username: 'Bret2222',
+    email: 'Sincere@april.biz',
+    address: {
+      street: 'Kulas Light',
+      suite: 'Apt. 556',
+      city: 'Gwenborough',
+      zipcode: '92998-3874',
+      geo: {
+        lat: '-37.3159',
+        lng: '81.1496',
+      },
+    },
+    phone: '1-770-736-8031 x56442',
+    website: 'hildegard.org',
+    company: {
+      name: 'Romaguera-Crona',
+      catchPhrase: 'Multi-layered client-server neural-net',
+      bs: 'harness real-time e-markets',
+    },
+  };
 
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(){
-    this.obtenerListaUsuarios();
+  ngOnInit() {
+    this.actualizarUsuario(1, this.usu).subscribe((data) =>{
+      console.log(data);
+    });
+
+    this.obtenerListaUsuarios().subscribe((data) => {
+      console.log(data);
+    });
+
+    this.obtenerUsuario(1).subscribe((data) => {
+      console.log(data);
+    });
+
+    this.eliminarUsuario(1).subscribe((data) =>{
+      console.log(data)
+    })
   }
 
-  obtenerListaUsuarios():Observable<usuarioI[]>{
-    debugger;
-    let url = "https://jsonplaceholder.typicode.com/users/"
-    var temp = this.http.get<usuarioI[]>(url); 
-    console.log(temp);
-    return temp;
+  obtenerListaUsuarios(): Observable<usuarioI[]> {
+    let urlObtenerListaUsuarios = this.url + 'users/';
+    return this.http.get<usuarioI[]>(urlObtenerListaUsuarios);
+  }
+
+  obtenerUsuario(id: number): Observable<usuarioI> {
+    let urlObtenerUsuarios = this.url + 'users/' + id;
+    return this.http.get<usuarioI>(urlObtenerUsuarios);
+  }
+
+  actualizarUsuario(id: number, usuario: usuarioI): Observable<usuarioI> {
+    let urlObtenerUsuarios = this.url + 'users/' + id;
+    return this.http.put<usuarioI>(urlObtenerUsuarios, usuario);
+  }
+
+  eliminarUsuario(id: number): Observable<string> {
+    let urlObtenerUsuarios = this.url + 'users/' + id;
+    return this.http.delete<string>(urlObtenerUsuarios);
   }
 }
